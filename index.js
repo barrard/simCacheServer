6f656b2e3d9fc8db9061984f54770acb05e69c65
David Barrar <barrard@gmail.com>
2021-01-27 12:03:19 -0800

    increased the limit of whats possible

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-var express = require('express')
+var express = require("express");
 
-var app = express()
+var app = express();
 
- 
-let data ={}
-app.use(express.json({limit:'1000mb'})) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+let data = {};
+app.use(express.json({ limit: "3000mb" })); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 // app.use(express.bodyParser({limit: '50mb'}));
-var cors = require('cors')
-app.use(cors())
+var cors = require("cors");
+app.use(cors());
 
 // respond with "hello world" when a GET request is made to the homepage
-app.post('/save', function (req, res) {
-	// console.log(req.body)
-	console.log('data saved')
-	data = req.body
-  res.json({res:'hello world'})
-})
+app.post("/save", function (req, res) {
+  // console.log(req.body)
+  console.log("data saved");
+  data = req.body;
+  res.json({ res: "hello world" });
+});
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/get', function (req, res) {
-	console.log('hit get')
-  res.json(data)
-})
+app.get("/get", function (req, res) {
+  console.log("hit get");
+  res.json(data);
+});
 
-
-const PORT = 1338
+const PORT = 1338;
 
 app.listen(PORT, function () {
-  console.log(`sim data cache running on port ${PORT}`)
-})
+  console.log(`sim data cache running on port ${PORT}`);
+});
